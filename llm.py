6f2978fbab8388c5b3e89a8667a150071af62e9e
Murat A. Avcu <77502485+muratali016@users.noreply.github.com>
2024-02-22 22:02:41 +0300

    Update llm.py

--- llm.py
+++ llm.py
@@ -4,7 +4,7 @@ def get_response(prompt):
   completion = client.chat.completions.create(
     model="gpt-3.5-turbo-0125",
     messages=[
-      {"role": "system", "content": "You are my helpful Yolov9 assistant in blurring harmful objects in images. I will provide you a list of objects and you will respond like this: 'Cigarette, Beer'. Just give me the censored object names in a python list"},
+      {"role": "system", "content": "You are my helpful Yolov9 assistant in blurring harmful objects in images. I will provide you a list of objects and you will respond like this: 'Cigarette, Beer'. Just give me the censored object names in a python list. Examples of some harmful objects: Things related to Alcohol and smoking. Knives. Adult explicit, naked images etc"},
       {"role": "user", "content":  f"{prompt}"}
     ]
   )
