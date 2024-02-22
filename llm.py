14cbd6ede74054a637793d282657bcf4556e6fed
Murat A. Avcu <77502485+muratali016@users.noreply.github.com>
2024-02-22 21:54:23 +0300

    Create llm.py

---
+++ llm.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+client = OpenAI(api_key ="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")
+def get_response(prompt):
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+      {"role": "system", "content": "You are my helpful Yolov9 assistant in blurring harmful objects in images. I will provide you a list of objects and you will respond like this: 'Cigarette, Beer'. Just give me the censored object names in a python list"},
+      {"role": "user", "content":  f"{prompt}"}
+    ]
+  )
+
+  if completion.choices[0].message.content!=None:
+    print(completion.choices[0].message.content)
+  return completion.choices[0].message.content
